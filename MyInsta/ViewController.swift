439f0957875f1ad87a4d55c7c4241fc314fd605c
Jose <josedavila@joses-mac.local>
2019-03-06 14:32:51 -0800

    Initial Commit

---
+++ MyInsta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyInsta
+//
+//  Created by Jose  Davila on 3/6/19.
+//  Copyright © 2019 Jose Davila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
