89f48ef32b0ea4af4f2f0c01d2941ef3db62bfc9
Jose <josedavila@joses-mac.local>
2019-03-13 23:52:21 -0700

    1. Added new table view cell to display new comments 2. Added logic to retrieve new comments to be displayed in the table view cell

---
+++ MyInsta/CommentCell.swift
@@ -0,0 +1,28 @@
+//
+//  CommentCell.swift
+//  MyInsta
+//
+//  Created by Jose  Davila on 3/13/19.
+//  Copyright © 2019 Jose Davila. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var commentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- MyInsta/FeedViewController.swift
+++ MyInsta/FeedViewController.swift
@@ -29,7 +29,7 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidAppear(animated)
         
         let query = PFQuery(className:"Posts")
-        query.includeKey("author")
+        query.includeKeys(["author", "comments", "comments.author"])
         query.limit = 20
         
         query.findObjectsInBackground { (posts, error) in
@@ -41,26 +41,46 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return posts.count
+        let post = posts[section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
+        
+        return comments.count + 1
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+            return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as! PostCell
         
-        let post = posts[indexPath.row]
+        let post = posts[indexPath.section]
+        let comments = (post["comments"] as? [PFObject]) ?? []
         
-        let user = post["author"] as! PFUser
-        cell.userNameLabel.text = user.username
+        if indexPath.row == 0 {
+            let user = post["author"] as! PFUser
+            cell.userNameLabel.text = user.username
         
-        cell.captionLabel.text = post["caption"] as? String
+            cell.captionLabel.text = post["caption"] as! String
         
-        let imageFile = post["image"] as! PFFileObject
-        let urlString = imageFile.url!
-        let url = URL(string: urlString)
+            let imageFile = post["image"] as! PFFileObject
+            let urlString = imageFile.url!
+            let url = URL(string: urlString)
         
-        cell.photoView.af_setImage(withURL: url!)
+            cell.photoView.af_setImage(withURL: url!)
         
-        return cell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "CommentCell") as! CommentCell
+            
+            let comment = comments[indexPath.row - 1]
+            cell.commentLabel.text = comment["text"] as? String
+            
+            let user = comment["author"] as! PFUser
+            cell.nameLabel.text = user.username
+            
+            return cell
+        }
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
@@ -91,7 +111,8 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     */
     
-    @IBAction func logoutButton(_ sender: Any) {
+    }
+    @IBAction func onLogout(_ sender: Any) {
         PFUser.logOut()
         
         let main = UIStoryboard(name: "Main", bundle: nil)
